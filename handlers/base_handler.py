b9a3b97bd8e693535566a9a4ac6ff8e4aca991a9
Jelmer Mulder <jelmer@jelmermulder.dev>
2020-11-07 20:03:04 +0100

    Fix CORS

--- handlers/base_handler.py
+++ handlers/base_handler.py
@@ -6,8 +6,12 @@ from vertical_life_client import VerticalLifeClient
 
 
 class BaseHandler(SessionMixin, tornado.web.RequestHandler, ABC):
+    def set_default_headers(self) -> None:
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header("Content-Type", "application/json")
+
     def initialize(self, vl_client: VerticalLifeClient):
         self.vl_client = vl_client
 
-    def prepare(self):
-        self.set_header("Content-Type", "application/json")
